bc84f033e186151cba84708bb83f25e73ad87e5d
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2016-11-17 00:26:40 +0100

    refine demo site : title, package.json, ...

--- site/src/app.ts
+++ site/src/app.ts
@@ -1,13 +1,8 @@
 export class App {
-    theme: string;
     language: string = "java";
     includePath: string = "src/code/sample.java"
     
     onLanguageChange() {
         this.includePath = "src/code/sample." + this.language;
-    }
-    
-    changeTheme() {
-        alert(this.theme);
     }
 }
